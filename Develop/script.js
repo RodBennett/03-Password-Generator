1521f1078c9cbe56a2ab1b71c305e634b9731930
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-07-11 07:04:30 -0600

    More targeted special characters

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alpha1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*"];
 var totalCharacters = [];
 
 
@@ -18,27 +18,27 @@ function generatePassword() {
 // User prompted to continue, choosing numbers, letters, etc.
     var confirmNumber = confirm("Would you like to use numbers in your password?");
       if (confirmNumber) {
-      confirm("Your password will contain numbers.");
+      confirm("Your password will contain numbers");
     } else {
-      confirm("Your password will not contain any numbers.");
+      confirm("Your password will not contain any numbers");
     }
     var confirmLower = confirm("Would you like to use lowercase letters?");
       if (confirmLower) {
-      confirm("Your password will contain a lower case letter");
+      confirm("Your password will contain lower case letters");
     } else {
       confirm("Your password will not contain a lower case letter");
     }
     var confirmUpper = confirm("Would you like to use upper case letters?");
       if (confirmUpper) {
-      confirm("Your password will contain an upper case letter.");
+      confirm("Your password will contain upper case letters");
     } else {
-      confirm("Your password will not contain an upper case letter.");
+      confirm("Your password will not contain upper case letters");
      }
-    var confirmSpecials = confirm("Would you like to use special characters? (!@#$%^&*)?");
+    var confirmSpecials = confirm("Would you like to use special characters? !@#$%^&*?");
       if (confirmSpecials) {
       confirm("Your password will contain special characters");
     } else {
-      confirm("Your password will not contain any special characters.");
+      confirm("Your password will not contain any special characters");
     }
 // If none of the 4 criteria are chosen, user recieves confirm to choose at least one criteria.
       if (confirmLower === false && confirmUpper === false && confirmNumber == false && confirmSpecials === false) {
